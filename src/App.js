73335033863dbb30ac994130ec643cdaf1aee17b
yunbiyomi <yunbiyomi@naver.com>
2023-08-23 23:06:16 +0900

    ✨ Feat : Router 설정

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
 import './App.css';
-import Banner from './components/Banner';
-import Category from './components/Category';
-import Nav from './components/Nav';
-import styled from 'styled-components'
-import Row from './components/Row';
-import requests from './api/request';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav'
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+
+const Layout = () => {
+  return(
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Nav/>
-        <Banner/>
-        <Category/>
-        <Row title="새로 개봉한 영화" id="TN" fetchUrl={requests.fetchTrending}></Row>
-        <Row title="TOP 영화" id="TR" fetchUrl={requests.fetchTopRated}></Row>
-        <Row title="액션 영화" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
-        <Row title="코미디 영화" id="CM" fetchUrl={requests.fetchComedyMovies}></Row>
-        <Row title="다큐멘터리 영화" id="DM" fetchUrl={requests.fetchDocumentaries}></Row>
-        <Row title="공포 영화" id="HM" fetchUrl={requests.fetchHorrorMovies}></Row>
-        <Row title="로맨스 영화" id="RM" fetchUrl={requests.fetchRomanceMovies}></Row>
-      </Container>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<LoginPage />}></Route>
+          <Route path='main' element={<MainPage />}></Route>
+          <Route path=':movieId' element={<DetailPage />}></Route>
+          <Route path='search' element={<DetailPage />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);
-  overflow-y: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-
-  &::after {
-    background: url('/images/home-background.png') center center / cover no-repeat fixed;
-    content: "";
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`;
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Banner from './components/Banner';
+import Category from './components/Category';
+import Nav from './components/Nav';
+import styled from 'styled-components'
+import Row from './components/Row';
+import requests from './api/request';
+
+const MainPage = () => {
+  return (
+    <div className="App">
+      <Container>
+        <Nav/>
+        <Banner/>
+        <Category/>
+        <Row title="새로 개봉한 영화" id="TN" fetchUrl={requests.fetchTrending}/>
+        <Row title="TOP 영화" id="TR" fetchUrl={requests.fetchTopRated}/>
+        <Row title="액션 영화" id="AM" fetchUrl={requests.fetchActionMovies}/>
+        <Row title="코미디 영화" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title="다큐멘터리 영화" id="DM" fetchUrl={requests.fetchDocumentariRow}/>
+        <Row title="공포 영화" id="HM" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title="로맨스 영화" id="RM" fetchUrl={requests.fetchRomanceMovies}/>
+      </Container>
+    </div>
+  );
+}
+
+export default MainPage;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-y: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    background: url('/images/home-background.png') center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
